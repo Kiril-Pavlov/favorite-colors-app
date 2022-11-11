f5dddfa344066684762a6f90a4b1038cfaeb1681
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-11 19:28:33 +0100

    added some styles to the title and the input area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {AiOutlineHeart} from "react-icons/ai"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="main-container">
+      <h1 className='main-title'>My Favoruite Colors</h1>
+      <div className='add-colors-container'>
+        <div className='input-container'>
+          <div className='heart-icon'>
+            <AiOutlineHeart fill='#D8D9D9' size={20}/>
+          </div>
+          <div className='input-field'>
+            <input type="text" placeholder='Color Name'/>
+          </div>
+          <div className='selected-color-container'>
+            <div className='display-selected-color'>
+
+            </div>
+          </div>
+        </div>
+        <div className='add-color-button'>
+
+        </div>
+      </div>
     </div>
   );
 }
